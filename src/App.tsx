55f92087e91d1a16ee720761e6dca61f0a770199
Karan Meghe <karanmeghe87@gmail.com>
2025-05-13 16:57:57 +0530

    feat: Providing Redux store for every components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 /** @format */
+import { Provider } from 'react-redux';
 import AppLayout from './pages/AppLayout';
+import { store } from './Redux/store';
 import { Header, SideBar } from './ui';
 
 function App() {
   return (
-    <div className='flex'>
-      <SideBar />
-      <div className='flex flex-col flex-1'>
-        <Header />
-        <AppLayout />
+    <Provider store={store}>
+      <div className='flex'>
+        <SideBar />
+        <div className='flex flex-col flex-1'>
+          <Header />
+          <AppLayout />
+        </div>
       </div>
-    </div>
+    </Provider>
   );
 }
 
