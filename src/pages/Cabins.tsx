33241daa628fe84eb27324c36778b5323d396656
Karan Meghe <karanmeghe87@gmail.com>
2025-05-14 14:31:36 +0530

    refactor: normal refactor

--- src/pages/Cabins.tsx
+++ src/pages/Cabins.tsx
@@ -1,8 +1,12 @@
 /** @format */
 
+import { useFetchCabinsQuery } from '../Redux/api/cabinsApi';
+
 function Cabins() {
+  const { data } = useFetchCabinsQuery();
+  console.log(data);
   return (
-    <div className='flex'>
+    <div className='flex justify-between px-2'>
       <h2>All cabins</h2>
       <p>TEST</p>
     </div>
